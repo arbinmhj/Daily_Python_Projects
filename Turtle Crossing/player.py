4acafe325443e9bed2166e65b4353caa6d8708ce
Arbin Maharjan <49609055+arbinmhj@users.noreply.github.com>
2022-08-30 15:59:39 +0545

    Add files via upload

---
+++ Turtle Crossing/player.py
@@ -0,0 +1,37 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.reset_position()
+        self.left(90)
+        self.color("red")
+
+    def go_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def go_left(self):
+        new_x = self.xcor() - MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
+
+    def go_right(self):
+        new_x = self.xcor() + MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
+
+    def reset_position(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
